e77b5f6552e44af355e00c5c8c235d2ff8871685
PepeXMaster <pepe3fragoso@gmail.com>
2023-10-26 18:58:55 -0600

    NovaleMadre
    sss

--- build/preproc/ctags_target_for_gcc_minus_e.cpp
+++ build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -53,8 +53,8 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(13, 0x1); // turn the LED on (HIGH is the voltage level)
+  digitalWrite(13, 0x0); // turn the LED on (HIGH is the voltage level)
   delay(1000); // wait for a second
-  digitalWrite(13, 0x0); // turn the LED off by making the voltage LOW
+  digitalWrite(13, 0x1); // turn the LED off by making the voltage LOW
   delay(1000); // wait for a second
 }
