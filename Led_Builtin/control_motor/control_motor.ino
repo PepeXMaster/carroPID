f69fbcf0bf2ac1f99435ea113dc213a58113e9c3
PepeXMaster <pepe3fragoso@gmail.com>
2023-10-28 13:35:54 -0600

    Comentarios
    Se crearon comentarios en el código, cuando puedan los checan

---
+++ Led_Builtin/control_motor/control_motor.ino
@@ -0,0 +1,60 @@
+int ENA = 11;
+int INA = 12;
+int INB = 13;
+int velocidad = 255; // Velocidad del motor que va de 0 a 255
+
+//Función para ir hacia adelante
+void adelante(){
+   analogWrite(ENA,velocidad);
+   digitalWrite(INA,HIGH);
+   digitalWrite(INB,LOW); 
+}
+
+//Función para ir hacia atrás 
+void atras(){
+    analogWrite(ENA,velocidad);
+    digitalWrite(INA,LOW);
+    digitalWrite(INB,HIGH);
+}
+
+//Función para detener el carro totalmente
+void detener(){
+    analogWrite(ENA,0);
+    digitalWrite(INA,LOW);
+    digitalWrite(INB,LOW);
+}
+
+void setup() {
+    pinMode(velocidad, OUTPUT);
+    pinMode(INA, OUTPUT);
+    pinMode(INB, OUTPUT);
+    delay(2000); // Que tarde 2 segundos en empezar el programa
+}
+
+   void loop() {
+    //Avanzar por 3 segundos
+    adelante();
+    delay(3000);
+
+    //Detenerse por 1 segundo
+    detener();
+    delay(1000);
+
+    //Retroceder por 3 segundos
+    atras();
+    delay(3000);
+
+    //Detenerse por 1 segundo
+    detener();
+    delay(1000);
+}
+/*
+A este código le hace falta confirmar que los pines INA e INB estén bien asignados, de lo contrario
+las funciones adelante y atrás estarán invertidas; para resolver esto solo inviertan los pines
+de INA e INB que se encuentra hasta arriba del código. 
+
+Este código también es para probar la mecánica, en teoría el carro debería terminar justo donde comenzó;
+en caso de que no entonces es porque la dirección no está totalmente derecha, o los engranajes se están 
+barriendo (se cree que cuando el encoder se incluya al programa, no importará si los engranes se barren,
+favor de confirmar este dato).
+*/
