aa2ef54df6a222c89ca4e259b6a35771a62ce98b
SalvadorP23 <149124827+SalvadorP23@users.noreply.github.com>
2023-11-01 17:30:52 -0600

    Update servo.ino
    Agregué la funcion del servo y los ángulos para las vueltas y recta.
    No son los definitivos PEPE, hay que hacer mas pruebas para encontrar los mejores o ver si lo hacemos con el PWM.

--- Led_Builtin/master/servo.ino
+++ Led_Builtin/master/servo.ino
@@ -1,11 +1,18 @@
 void derecha(){
-
+  angulogiro(42);
 }
 
 void izquierda(){
-
+  angulogiro(5);
 }
 
 void recto(){
-
+  angulogiro(27);
 }
+
+//Funcion servo
+void angulogiro(int angulo) {
+  if (angulo >= 0 && angulo <= 180) {
+    servodireccion.write(angulo);
+    delay(1000);
+  }
